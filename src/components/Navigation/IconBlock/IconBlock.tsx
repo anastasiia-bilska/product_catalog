e110f0034241fa000625ef57139c0d518f499f65
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-03-06 10:01:16 +0200

    icons update

--- src/components/Navigation/IconBlock/IconBlock.tsx
+++ src/components/Navigation/IconBlock/IconBlock.tsx
@@ -6,10 +6,11 @@ import { NavLink, To } from 'react-router-dom';
 interface Props {
   iconName: string;
   to: To;
-  setIOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsMenuOpened: React.Dispatch<React.SetStateAction<boolean>>;
+  isMenuOpened?: boolean,
 }
 
-export const IconBlock: React.FC<Props> = React.memo(({ iconName, to }) => (
+export const IconBlock: React.FC<Props> = React.memo(({ iconName, to, setIsMenuOpened }) => (
   <div className="iconBlock">
     <NavLink
       title={iconName}
@@ -18,6 +19,7 @@ export const IconBlock: React.FC<Props> = React.memo(({ iconName, to }) => (
       className={({ isActive }) => cn('iconBlock__link', {
         'iconBlock__link--active': isActive,
       })}
+      onClick={() => setIsMenuOpened(false)}
     >
       <div className={`iconBlock__icon iconBlock__icon--${iconName}`}></div>
     </NavLink>
